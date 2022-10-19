0296954b52443be8fa00ce0aaccd3cc623e79c54
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2022-10-19 18:52:31 +0530

    Update PaymentController.php

--- src/Controllers/PaymentController.php
+++ src/Controllers/PaymentController.php
@@ -143,7 +143,7 @@ class PaymentController extends Controller
             $this->getLogger(__METHOD__)->error('response', $paymentResponseData);
             // Set the payment response in the session for the further processings
             $this->sessionStorage->getPlugin()->setValue('nnPaymentData', array_merge($paymentRequestData, $paymentResponseData));
-            if($this->settingsService->getPaymentSettingsValue('novalnet_order_creation') != true) {
+            if($this->settingsService->getPaymentSettingsValue('novalnet_order_creation') != true && !isset($paymentResponseData['transaction']['order_no'])) {
                 // Call the shop executePayment function
                 return $this->response->redirectTo($this->sessionStorage->getLocaleSettings()->language . '/place-order');
             }
